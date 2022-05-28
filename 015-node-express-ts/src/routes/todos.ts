1552a834794c573333ba24935e9a934aeef142d5
Aleksandar Ristic <storiaca@gmail.com>
2022-05-28 17:29:24 +0200

    Fix typo

--- 015-node-express-ts/src/routes/todos.ts
+++ 015-node-express-ts/src/routes/todos.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 
-import { createTodo, getTodos, updateTodo, deleteTodo } from "../controllers/todos";
+import {
+  createTodo,
+  getTodos,
+  updateTodo,
+  deleteTodo,
+} from "../controllers/todos";
 
 const router = Router();
 
 router.post("/", createTodo);
 
-router.get("/", getTodos;
+router.get("/", getTodos);
 
 router.patch("/:id", updateTodo);
 
