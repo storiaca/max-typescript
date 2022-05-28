50bc7f3293fdbb71900bffc855258e7d5ef0f4a4
Aleksandar Ristic <storiaca@gmail.com>
2022-05-28 15:44:35 +0200

    Workimg with controllers and parsing request bodies

--- 015-node-express-ts/src/app.ts
+++ 015-node-express-ts/src/app.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
+import { json } from "body-parser";
 
 import todoRoutes from "./routes/todos";
 
 const app = express();
+
+app.use(json());
 
 app.use("/todos", todoRoutes);
 
@@ -10,4 +13,4 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 });
 
-app.listen(3000);
+app.listen(3030);
