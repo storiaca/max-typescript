585e5a2ad313110d726ffca034334a8a6f25a0c2
Aleksandar Ristic <storiaca@gmail.com>
2025-01-09 09:25:14 +0100

    The never Type

--- 002-ts-basics-types/app.js
+++ 002-ts-basics-types/app.js
@@ -1,19 +1,9 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+// userName = userInput; // error
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-console.log(combineValues(8, 8));
-// let someValue: undefined;
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+function generateError(message, code) { }
