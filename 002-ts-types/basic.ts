798e0e72113416dd28f49669af6639796e1ddbb4
Aleksandar Ristic <storiaca@gmail.com>
2025-03-05 14:40:59 +0100

    asigning Types to functions parameters

--- 002-ts-types/basic.ts
+++ 002-ts-types/basic.ts
@@ -7,7 +7,16 @@ console.log("Hello TypeScript!");
 
 /* Types */
 // variables: string, number, boolean
-let userName: string;
+// let userName: string;
 let userAge = 38;
 
 userName = "Aleksandar";
+// userAge = '34'
+
+// function parameters example
+function add(a: number, b = 5) {
+  return a + b;
+}
+
+add(10);
+// moramo da prosledimo broj kao parameta
