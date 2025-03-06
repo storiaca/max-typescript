1c9cb1d7a45918ff898d9f8ff79747e72017dba2
Aleksandar Ristic <storiaca@gmail.com>
2025-03-06 09:57:22 +0100

    Advanced array types

--- 002-ts-types/arrays-objects.ts
+++ 002-ts-types/arrays-objects.ts
@@ -4,3 +4,8 @@ let hobbies = ["Sports", "Cooking"];
 // ako probamo da dodamo broj u niz dobicemo gresku
 // znaci da je ts vec odredio da je niz koji ima samo tipove string: hobbies: string[]
 // hobbies.push(10);
+
+// mozemo da definsemo kojeg ce tipa da budu vrednosti u nizu na primer stingove i brojeve:
+let users: (string | number)[];
+
+users = [1, "Max"];
