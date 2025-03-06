ad9522c2e222bfcee7847c5bc43b88d2a223812f
Aleksandar Ristic <storiaca@gmail.com>
2025-03-06 09:50:21 +0100

    Arrays and types

---
+++ 002-ts-types/arrays-objects.js
@@ -0,0 +1,5 @@
+// arrays
+var hobbies = ["Sports", "Cooking"];
+// ako probamo da dodamo broj u niz dobicemo gresku
+// znaci da je ts vec odredio da je niz koji ima samo tipove string: hobbies: string[]
+// hobbies.push(10);
---
+++ 002-ts-types/arrays-objects.ts
@@ -0,0 +1,6 @@
+// arrays
+let hobbies = ["Sports", "Cooking"];
+
+// ako probamo da dodamo broj u niz dobicemo gresku
+// znaci da je ts vec odredio da je niz koji ima samo tipove string: hobbies: string[]
+// hobbies.push(10);
