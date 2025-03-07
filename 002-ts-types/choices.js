d6e294d65c4e06146eb1100d113447cc1d67aa57
Aleksandar Ristic <storiaca@gmail.com>
2025-03-07 10:53:51 +0100

    Being specific with literal types

--- 002-ts-types/choices.js
+++ 002-ts-types/choices.js
@@ -7,7 +7,16 @@ var Role;
 })(Role || (Role = {}));
 // ako uradimo Role. dobicemo predlog koje vrednosti imamo u Role
 var userRole = Role.Admin;
-// ...
+// kasnije...
 userRole = Role.Guest;
 // mozemo da koristimo i brojeve kao vrednosti na primer: let userRole: Role = 0; ovo je validno zato sto ce da se odnosi na Admin, ako stavimo neki veci broj kao 5 onda ce da javi gresku
 // mada mozemo mi da dodelimo brojeve vrednostima, ako stavimo da je Admin = 1, onda ce brojanje da krene od 1: enum Role { Admin = 1, Editor, Guest}
+/* alternativa za enum moze da bude literal tip */
+var roleUser = "admin";
+// kasnije...
+roleUser = "guest";
+// mozemo da korigujemo tuple iz predhodnog primera, tako sto cemo da koristimo literal tip
+// let possibleResults: [number, number] // [1, -1]
+// possibleResults = [1, -1]
+var possibleResults;
+possibleResults = [1, -1];
--- 002-ts-types/choices.ts
+++ 002-ts-types/choices.ts
@@ -7,8 +7,21 @@ enum Role {
 
 // ako uradimo Role. dobicemo predlog koje vrednosti imamo u Role
 let userRole: Role = Role.Admin;
-// ...
+// kasnije...
 userRole = Role.Guest;
 
 // mozemo da koristimo i brojeve kao vrednosti na primer: let userRole: Role = 0; ovo je validno zato sto ce da se odnosi na Admin, ako stavimo neki veci broj kao 5 onda ce da javi gresku
 // mada mozemo mi da dodelimo brojeve vrednostima, ako stavimo da je Admin = 1, onda ce brojanje da krene od 1: enum Role { Admin = 1, Editor, Guest}
+
+/* alternativa za enum moze da bude literal tip */
+
+let roleUser: "admin" | "editor" | "guest" = "admin";
+
+// kasnije...
+roleUser = "guest";
+
+// mozemo da korigujemo tuple iz predhodnog primera, tako sto cemo da koristimo literal tip
+// let possibleResults: [number, number] // [1, -1]
+// possibleResults = [1, -1]
+let possibleResults: [1 | -1, 1 | -1];
+possibleResults = [1, -1];
