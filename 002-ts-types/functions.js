6db61582ec2870735d0ae749fac35063a849f9f3
Aleksandar Ristic <storiaca@gmail.com>
2025-03-09 19:45:06 +0100

    The 'never' type

--- 002-ts-types/functions.js
+++ 002-ts-types/functions.js
@@ -2,6 +2,13 @@
 function add(a, b) {
     return a + b;
 }
+// void return
 function log(message) {
     console.log(message);
+}
+// "never" type
+// never znaci da se ova funkcija nikada nece zavrsiti jer baca gresku
+function logAndThrow(errorMessage) {
+    console.log(errorMessage);
+    throw new Error(errorMessage);
 }
--- 002-ts-types/functions.ts
+++ 002-ts-types/functions.ts
@@ -3,6 +3,14 @@ function add(a: number, b: number): number {
   return a + b;
 }
 
+// void return
 function log(message: string): void {
   console.log(message);
+}
+
+// "never" type
+// never znaci da se ova funkcija nikada nece zavrsiti jer baca gresku
+function logAndThrow(errorMessage: string): never {
+  console.log(errorMessage);
+  throw new Error(errorMessage);
 }
