abbe22444be38f170c4a7e9a778e00eb07e642e5
Aleksandar Ristic <storiaca@gmail.com>
2025-03-11 12:14:54 +0100

    null and undefined - special types

---
+++ 002-ts-types/special-types.js
@@ -0,0 +1,10 @@
+// null i undefined tipovi
+// null moze da bude korisno kad se kombinuje u union tipovima
+var a;
+a = "Hi!";
+//....
+a = null;
+var b;
+b = "Hi!";
+//....
+b = undefined;
---
+++ 002-ts-types/special-types.ts
@@ -0,0 +1,15 @@
+// null i undefined tipovi
+// null moze da bude korisno kad se kombinuje u union tipovima
+let a: null | string;
+
+a = "Hi!";
+//....
+
+a = null;
+
+let b: undefined | string;
+
+b = "Hi!";
+//....
+
+b = undefined;
