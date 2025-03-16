bfc7175f7ed39268ed3844e30abfcf7955ec6252
Aleksandar Ristic <storiaca@gmail.com>
2025-03-16 14:00:46 +0100

    Getting started and first steps

---
+++ 004-ts-demo-project/calculator.ts
@@ -0,0 +1,16 @@
+/**
+ * @param data:
+ * initial amount
+ * annual contibution
+ * expected return
+ * duration
+ */
+function calculateInvestement(data) {} // => result[]
+
+function printResults(results) {
+  // print (output) the result data
+}
+
+const results = calculateInvestement(..);
+
+printResults(results)
