80917d5385be1331a153abf8c0071bc5e1a6a5f9
Aleksandar Ristic <storiaca@gmail.com>
2025-03-17 13:06:00 +0100

    Adding Application logic

--- 004-ts-demo-project/calculator.ts
+++ 004-ts-demo-project/calculator.ts
@@ -15,13 +15,49 @@ type InvestmentData = {
 type InvestmentResult = {
   year: string;
   totalAmount: number;
-  totalContrubutions: number;
+  totalContributions: number;
   totalInterestEarned: number;
 }
 
-type CalculationResult = InvestmentResult | string;
+type CalculationResult = InvestmentResult[] | string;
 
-function calculateInvestement(data: InvestmentData): CalculationResult {} // => result[]
+function calculateInvestement(data: InvestmentData): CalculationResult {
+  const { initialAmount, annualContribution, expectedReturn, duration } = data;
+  if (initialAmount < 0) {
+    return 'Initial investment amount must be at least zero.';
+  }
+
+  if (duration <= 0) {
+    return 'No valid amount of years provided.';
+  }
+
+  if (expectedReturn <= 0) {
+    return 'expected return must be at least zero.';
+  }
+
+  let total = initialAmount;
+  let totalContributions = 0;
+  let totalInterestEarned = 0;
+
+  const annualResults: InvestmentResult[] = [];
+
+  for (let i = 0; i < duration; i++) {
+    total = total * (1 + expectedReturn);
+    totalInterestEarned = total - totalContributions - initialAmount;
+    totalContributions = totalContributions + annualContribution;
+    total = total + annualContribution;
+
+    annualResults.push({
+      year: `Year ${i + 1}`,
+      totalAmount: total,
+      totalInterestEarned,
+      totalContributions
+    });
+  }
+
+  return annualResults;
+
+} // => result[]
 
 function printResults(results) {
   // print (output) the result data
