0357be564567a18d926f952a1792d0f3a18b5831
Aleksandar Ristic <storiaca@gmail.com>
2025-03-18 10:11:40 +0100

    let vs const

--- 005-next-gen-js-ts/README.md
+++ 005-next-gen-js-ts/README.md
@@ -1,1 +1,5 @@
 # Section 5: Next-generation JavaScript & TypeScript
+
+# Compat table
+
+https://compat-table.github.io/compat-table/es6/
--- 005-next-gen-js-ts/src/app.ts
+++ 005-next-gen-js-ts/src/app.ts
@@ -1,2 +1,18 @@
 // Code goes here!
-console.log("Hello");
+const userName = "Max";
+// userName = 'Maximilian';
+let age = 30;
+
+age = 27;
+
+// var result;
+let result: number = 0; // ovde mora da se dodeli neka vrednost za result inace ce da javlja gresku u liniji 18
+
+function add(a: number, b: number) {
+  // let result;
+  result = a + b;
+  return result;
+}
+
+add(6, 3);
+console.log(result);
