e0127761c1d997b174201755096af82ad523301e
Aleksandar Ristic <storiaca@gmail.com>
2025-03-18 10:26:56 +0100

    Arrow functions

--- 005-next-gen-js-ts/src/app.ts
+++ 005-next-gen-js-ts/src/app.ts
@@ -16,3 +16,21 @@ function add(a: number, b: number) {
 
 add(6, 3);
 console.log(result);
+
+// arrow functions
+
+const add2 = (a: number, b: number) => {
+  return a + b;
+};
+
+console.log(add2(32, 67));
+
+const printOutput = (output: string | number) => console.log(output);
+
+printOutput(add2(54, 28));
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
+}
