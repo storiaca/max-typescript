b0c6b071c49e78d61e80160a2ef2cf9153fdd0f6
Aleksandar Ristic <storiaca@gmail.com>
2025-03-20 12:33:57 +0100

    A Useful TypeScript Shortcut & Compiling to JavaScript

--- 006-class-interface/basics.js
+++ 006-class-interface/basics.js
@@ -1,19 +1,21 @@
+"use strict";
 // class User {
 //   name: string;
 //   age: number;
-
 //   constructor(n: string, a: number) {
 //     this.name = n;
 //     this.age = a;
 //   }
 // }
-
 // drugi nacin da kreiramo klasu na kraci nacin
 class User {
-  constructor(public name: string, public age: number) {}
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 }
-
 const aca = new User("Aca", 39);
 const fred = new User("Fred", 27);
-
 console.log(aca, fred);
