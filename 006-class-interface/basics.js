a8a21af803d13f14a340d7dc322dd17e5611e5a5
Aleksandar Ristic <storiaca@gmail.com>
2025-03-20 15:48:00 +0100

    Making sense of public and private

--- 006-class-interface/basics.js
+++ 006-class-interface/basics.js
@@ -11,11 +11,17 @@
 class User {
     name;
     age;
+    hobbies = [];
     constructor(name, age) {
         this.name = name;
         this.age = age;
     }
+    greet() {
+        console.log("My age: " + this.age);
+    }
 }
 const aca = new User("Aca", 39);
 const fred = new User("Fred", 27);
+// aca.age = 41; // ako je age public, mozemo da mu pristupimo, ako bi bilo private, vracalo bi gresku, zato sto ce imati pristup samo u klasi
+console.log(aca.greet());
 console.log(aca, fred);
--- 006-class-interface/basics.ts
+++ 006-class-interface/basics.ts
@@ -10,10 +10,23 @@
 
 // drugi nacin da kreiramo klasu na kraci nacin
 class User {
-  constructor(public name: string, public age: number) {}
+  public hobbies: string[] = [];
+  constructor(public name: string, private age: number) {}
+
+  greet() {
+    console.log("My age: " + this.age);
+  }
 }
 
 const aca = new User("Aca", 39);
 const fred = new User("Fred", 27);
+
+// aca.age = 41; // ako je age public, mozemo da mu pristupimo, ako bi bilo private, vracalo bi gresku, zato sto ce imati pristup samo u klasi
+
+// public i private se nalaze samo u ts-u, nema ih u vanila js-u
+// mada js ima privatne variable ako dodamo # ispred variable: #privateField
+// public je default ako ne navedemo, mada u konstruktoru uvek treba da navedemo public ili private
+
+console.log(aca.greet());
 
 console.log(aca, fred);
