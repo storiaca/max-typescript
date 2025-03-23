aa3225cbd89ee644ab111b176c5f9e342bdc4485
Aleksandar Ristic <storiaca@gmail.com>
2025-03-23 18:51:26 +0100

    setting Values with setters

--- 006-class-interface/advanced.js
+++ 006-class-interface/advanced.js
@@ -1,14 +1,26 @@
 "use strict";
 class UserName {
-    firstname;
-    lastName;
-    constructor(firstname, lastName) {
-        this.firstname = firstname;
-        this.lastName = lastName;
+    _firstname = "";
+    _lastName = "";
+    //constructor(private firstname: string, private lastName: string) {}
+    set firstname(name) {
+        if (name.trim() === "") {
+            throw new Error("Invalid name");
+        }
+        this._firstname = name;
+    }
+    set lastName(name) {
+        if (name.trim() === "") {
+            throw new Error("Invalid last name");
+        }
+        this._lastName = name;
     }
     get fullName() {
-        return this.firstname + " " + this.lastName;
+        return this._firstname + " " + this._lastName;
     }
 }
-const john = new UserName("John", "Ristic");
+// const john = new UserName("John", "Ristic");
+const john = new UserName();
+john.firstname = "John";
+john.lastName = "Petrovic";
 console.log(john.fullName);
--- 006-class-interface/advanced.ts
+++ 006-class-interface/advanced.ts
@@ -1,11 +1,31 @@
 class UserName {
-  constructor(private firstname: string, private lastName: string) {}
+  private _firstname: string = "";
+  private _lastName: string = "";
+  //constructor(private firstname: string, private lastName: string) {}
+
+  set firstname(name: string) {
+    if (name.trim() === "") {
+      throw new Error("Invalid name");
+    }
+    this._firstname = name;
+  }
+
+  set lastName(name: string) {
+    if (name.trim() === "") {
+      throw new Error("Invalid last name");
+    }
+    this._lastName = name;
+  }
 
   get fullName() {
-    return this.firstname + " " + this.lastName;
+    return this._firstname + " " + this._lastName;
   }
 }
 
-const john = new UserName("John", "Ristic");
+// const john = new UserName("John", "Ristic");
 
+const john = new UserName();
+
+john.firstname = "John";
+john.lastName = "Petrovic";
 console.log(john.fullName);
