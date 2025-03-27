e12866d0ae77d4506311a5440babdb0fa8606992
Aleksandar Ristic <storiaca@gmail.com>
2025-03-27 11:45:47 +0100

    Discriminated Unions

--- 007-advanced-types/guard.ts
+++ 007-advanced-types/guard.ts
@@ -1,10 +1,12 @@
-type FileSource = { path: string };
+type FileSource = { type: "file"; path: string };
 const fileSource: FileSource = {
+  type: "file",
   path: "some/path/to/file.csv",
 };
 
-type DBSource = { connectionUrl: string };
+type DBSource = { type: "db"; connectionUrl: string };
 const dbSource: DBSource = {
+  type: "db",
   connectionUrl: "some-connection-url",
 };
 
@@ -12,9 +14,12 @@ type Source = FileSource | DBSource;
 
 function loadData(source: Source) {
   // Open + read file OR reach out to database server
-  if ("path" in source) {
+  // if ("path" in source) {
+  if (source.type === "file") {
+    // source.path
     // source.parh; => use that to open the file
     return;
   }
+  // source.connectionUrl
   // source.connectionUrl; => to reach out to database
 }
