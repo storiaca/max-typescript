6e9e7910670a5ddad5c0f63a027a84afaee3dca1
Aleksandar Ristic <storiaca@gmail.com>
2025-03-27 11:55:27 +0100

    Type guards via instanceof

--- 007-advanced-types/guard.ts
+++ 007-advanced-types/guard.ts
@@ -1,3 +1,30 @@
+// type FileSource = { type: "file"; path: string };
+// const fileSource: FileSource = {
+//   type: "file",
+//   path: "some/path/to/file.csv",
+// };
+
+// type DBSource = { type: "db"; connectionUrl: string };
+// const dbSource: DBSource = {
+//   type: "db",
+//   connectionUrl: "some-connection-url",
+// };
+
+// type Source = FileSource | DBSource;
+
+// function loadData(source: Source) {
+//   // Open + read file OR reach out to database server
+//   // if ("path" in source) {
+//   if (source.type === "file") {
+//     // source.path
+//     // source.parh; => use that to open the file
+//     return;
+//   }
+//   // source.connectionUrl
+//   // source.connectionUrl; => to reach out to database
+// }
+
+/* Updated guard.ts */
 type FileSource = { type: "file"; path: string };
 const fileSource: FileSource = {
   type: "file",
@@ -13,13 +40,42 @@ const dbSource: DBSource = {
 type Source = FileSource | DBSource;
 
 function loadData(source: Source) {
-  // Open + read file OR reach out to database server
-  // if ("path" in source) {
+  // if ('path' in source) {
   if (source.type === "file") {
     // source.path
-    // source.parh; => use that to open the file
+    // source.path; => use that to open the file
     return;
   }
-  // source.connectionUrl
   // source.connectionUrl; => to reach out to database
+}
+
+class User {
+  constructor(public name: string) {}
+
+  join() {
+    // ...
+  }
+}
+
+class Admin {
+  constructor(permissions: string[]) {}
+
+  scan() {
+    // ...
+  }
+}
+
+const user = new User("Max");
+const admin = new Admin(["ban", "restore"]);
+
+type Entity = User | Admin;
+
+function init(entity: Entity) {
+  // .join() OR .scan() ...
+  if (entity instanceof User) {
+    entity.join();
+    return;
+  }
+
+  entity.scan();
 }
