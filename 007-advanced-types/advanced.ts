85a0cccaea9941e4528e56c693a91cf9a5c780b0
Aleksandar Ristic <storiaca@gmail.com>
2025-04-01 15:44:21 +0200

    Constant types with as const

--- 007-advanced-types/advanced.ts
+++ 007-advanced-types/advanced.ts
@@ -10,4 +10,6 @@ store.isOpen = false;
 // store.name = "Max"; // ovo je greksa jer nije dobar tip vrednosti
 
 // arrays
-let roles = ["admin", "guest", "editor"];
+let roles = ["admin", "guest", "editor"] as const; // ovo as const znaci da je niz readonly, ne moze da se dodaje, edituje
+
+//roles.push('max'); // greska
