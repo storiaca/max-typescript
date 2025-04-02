f611424e03dadac5eae99f884b8449883416bc4d
Aleksandar Ristic <storiaca@gmail.com>
2025-04-02 13:56:54 +0200

    The satisfies keyword

--- 007-advanced-types/advanced.ts
+++ 007-advanced-types/advanced.ts
@@ -15,3 +15,20 @@ store.isOpen = false;
 let roles = ["admin", "guest", "editor"] as const; // ovo as const znaci da je niz readonly, ne moze da se dodaje, edituje
 
 //roles.push('max'); // greska
+
+// satisfies
+// const dataEntries: Record<string, number> = {
+//   entry1: 0.36,
+//   entry2: 2.54,
+// };
+
+const dataEntries = {
+  entry1: 0.36,
+  entry2: 2.54,
+} satisfies Record<string, number>;
+
+// ...
+dataEntries.entry1;
+// dataEntries.entry3; // ovo ce da bude greksa sa satisfies zato sto ce ts da gleda kljuceve da li postoje u objektu
+// react router koristi ovu rec u routing delu, kada definisemo rute
+// https://reactrouter.com/start/framework/routing
