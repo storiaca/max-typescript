0d1f1f195907b2c801a5a6a7996ded43facb9d22
Aleksandar Ristic <storiaca@gmail.com>
2025-04-04 11:00:58 +0200

    Creating and using a generic types

--- 008-generics/generics.ts
+++ 008-generics/generics.ts
@@ -1,1 +1,16 @@
 let names: Array<string> = ["Aleksandar", "Max"];
+
+// type DataStore = {
+//   [key: string]: string | number;
+// };
+
+type DataStore<T> = {
+  [key: string]: T;
+};
+
+let store: DataStore<string | boolean> = {};
+
+store.name = "John";
+store.isInstructor = true; // ovo ce da vrati gresku, zato moramo da napravimo da DataStore bude fleksibilnija, tako sto cemo da koristimo generics
+
+let nameStore: DataStore<string> = {};
