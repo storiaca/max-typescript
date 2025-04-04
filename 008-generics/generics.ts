1969ae7dfbe42ccbbae6f96f7f79f0301845b6c6
Aleksandar Ristic <storiaca@gmail.com>
2025-04-04 11:17:07 +0200

    Working with Multiple Generic Parameters

--- 008-generics/generics.ts
+++ 008-generics/generics.ts
@@ -21,3 +21,10 @@ function merge<T>(a: T, b: T) {
 }
 
 const ids = merge<number>(1, 2); // u ovom slucaju mozemo da izostavimo ovo <number> zato sto ce ts da prepozna koji su tipovi argumenti
+
+// da imamo vise tipova kao parametre
+function merge2<T, U>(a: T, b: U) {
+  return [a, b];
+}
+
+const ids2 = merge2(1, "Aleksandar");
