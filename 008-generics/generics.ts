225bd1489cbba638d466fd3e920cb88bf3ce9160
Aleksandar Ristic <storiaca@gmail.com>
2025-04-04 11:12:14 +0200

    Generic functions and interfaces

--- 008-generics/generics.ts
+++ 008-generics/generics.ts
@@ -11,6 +11,13 @@ type DataStore<T> = {
 let store: DataStore<string | boolean> = {};
 
 store.name = "John";
-store.isInstructor = true; // ovo ce da vrati gresku, zato moramo da napravimo da DataStore bude fleksibilnija, tako sto cemo da koristimo generics
+store.isInstructor = true; // ovo ce da vrati gresku, zato moramo da napravimo da DataStore bude fleksibilnija, tako sto cemo da koristimo generic
 
 let nameStore: DataStore<string> = {};
+
+// mozemo da gradimo i generic funkcije
+function merge<T>(a: T, b: T) {
+  return [a, b];
+}
+
+const ids = merge<number>(1, 2); // u ovom slucaju mozemo da izostavimo ovo <number> zato sto ce ts da prepozna koji su tipovi argumenti
