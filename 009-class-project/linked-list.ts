3fe9cb86a8d17211f08583e9bf197bb7044aeb3a
Aleksandar Ristic <storiaca@gmail.com>
2025-04-06 12:33:35 +0200

    Adding an add Method

--- 009-class-project/linked-list.ts
+++ 009-class-project/linked-list.ts
@@ -1,10 +1,25 @@
-class ListNode {}
+class ListNode<T> {
+  next?: ListNode<T>;
+  constructor(public value: T) {}
+}
 
 class LinkedList<T> {
-  private root?: ListNode;
+  private root?: ListNode<T>;
   private length = 0;
 
-  add(value: T) {}
+  add(value: T) {
+    const node = new ListNode(value);
+    if (!this.root) {
+      this.root = node;
+    } else {
+      let current = this.root;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.length++;
+  }
 }
 
 const numberList = new LinkedList<number>();
