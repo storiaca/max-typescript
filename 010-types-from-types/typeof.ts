9ba4f14f907410c66caf14d8daaea8f4312d557b
Aleksandar Ristic <storiaca@gmail.com>
2025-04-07 13:19:47 +0200

    Typeof and a more useful example

--- 010-types-from-types/typeof.ts
+++ 010-types-from-types/typeof.ts
@@ -3,3 +3,26 @@ let userName = "Aleksandar";
 console.log(typeof userName);
 
 type UserName = typeof userName;
+
+const settings = {
+  difficulty: "easy",
+  minLevel: 10,
+  didStart: false,
+  players: ["John", "Jane"],
+};
+
+// prvi nacin
+type Settings = {
+  difficulty: string;
+  minLevel: number;
+  didStart: boolean;
+  players: string[];
+};
+// drugi nacin
+type Settings = typeof settings;
+
+function loadData(settings: Settings) {
+  // ...
+}
+
+loadData(settings);
