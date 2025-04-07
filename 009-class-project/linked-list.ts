0d70e19df931d1e9bac387269980eefff4a67e4d
Aleksandar Ristic <storiaca@gmail.com>
2025-04-07 12:56:12 +0200

    Finishing the linked list

--- 009-class-project/linked-list.ts
+++ 009-class-project/linked-list.ts
@@ -1,5 +1,6 @@
 class ListNode<T> {
   next?: ListNode<T>;
+
   constructor(public value: T) {}
 }
 
@@ -15,9 +16,58 @@ class LinkedList<T> {
       this.tail = node;
     } else {
       this.tail.next = node;
+      this.tail = node;
     }
     this.length++;
   }
+
+  insertAt(value: T, pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current = this.root;
+      let index = 0;
+      let previous = current;
+      let node = new ListNode(value);
+
+      if (pos === 0) {
+        node.next = this.root;
+        this.root = node;
+      } else {
+        while (index++ < pos && current.next) {
+          previous = current;
+          current = current.next;
+        }
+        node.next = current;
+        previous.next = node;
+      }
+      this.length++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  removeAt(pos: number) {
+    if (pos > -1 && pos < this.length && this.root) {
+      let current = this.root;
+      let previous: ListNode<T> = current;
+      let index = 0;
+
+      if (pos === 0) {
+        this.root = current.next;
+      } else {
+        while (index++ < pos && current.next) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      this.length--;
+      return current;
+    } else {
+      return null;
+    }
+  }
+
   getNumberOfElements() {
     return this.length;
   }
@@ -34,10 +84,20 @@ class LinkedList<T> {
 const numberList = new LinkedList<number>();
 
 numberList.add(10);
-numberList.add(4);
+numberList.add(5);
 numberList.add(-3);
 
-console.log(numberList.getNumberOfElements());
+console.log("Length: " + numberList.getNumberOfElements());
+numberList.print();
+
+console.log("--- NOW REMOVING INDEX 1 ---");
+numberList.removeAt(1);
+console.log("Length: " + numberList.getNumberOfElements());
+numberList.print();
+
+console.log("--- NOW INSERTING AT INDEX 1 ---");
+numberList.insertAt(100, 1);
+console.log("Length: " + numberList.getNumberOfElements());
 numberList.print();
 
 const nameList = new LinkedList<string>();
