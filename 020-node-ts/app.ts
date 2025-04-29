51f827341a3a14fa07f50fb76874a6dc998bb31c
Aleksandar Ristic <storiaca@gmail.com>
2025-04-29 10:41:18 +0200

    first server and working with  Node Types

--- 020-node-ts/app.ts
+++ 020-node-ts/app.ts
@@ -1,1 +1,8 @@
-console.log("Hello world");
+import { createServer } from "node:http";
+
+const server = createServer((req, res) => {
+  console.log(req.method);
+  res.end("Hello World");
+});
+
+server.listen(8080);
