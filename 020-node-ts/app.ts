9556f0f733731cd87b396666921c2ef6b258d182
Aleksandar Ristic <storiaca@gmail.com>
2025-04-30 13:40:42 +0200

    Finishing the basic app

--- 020-node-ts/app.ts
+++ 020-node-ts/app.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import router from "./routes/todo.js";
 
 const app = express();
 
 app.use(express.json());
+
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: "An error occured" });
+});
 
 app.listen(8080);
