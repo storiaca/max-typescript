2ae00a3a022e51ac4904d18ca187dc3960fcefcf
Aleksandar Ristic <storiaca@gmail.com>
2025-05-09 13:50:44 +0200

    Validation with Decorators begin

--- 012-experimental-decorators/src/app.ts
+++ 012-experimental-decorators/src/app.ts
@@ -128,3 +128,41 @@ const p = new Printer();
 const button = document.querySelector("button")!;
 
 button.addEventListener("click", p.showMessage);
+
+// ----
+
+function Required() {}
+
+function PositiveNumber() {}
+
+function validate(obj: object) {}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    alert("Invalid input, please try again!");
+    return;
+  }
+  console.log(createdCourse);
+});
