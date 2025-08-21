87124602d07438e9293d2a962ac8be94c2a02b25
Aleksandar Ristic <storiaca@gmail.com>
2025-08-21 18:52:57 +0200

    Utilizing Interfaces to Implement Drag & Drop

--- 013-project-drag/src/app.ts
+++ 013-project-drag/src/app.ts
@@ -1,3 +1,15 @@
+/* Drag & Drop Interfaces */
+interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
+}
+
 /* Project Type */
 enum ProjectStatus {
   Active,
@@ -161,7 +173,10 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 /* ProjectList Class */
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
   private project: Project;
 
   get persons() {
@@ -180,7 +195,19 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
     this.renderContent();
   }
 
-  configure() {}
+  @AutoBind
+  dragStartHandler(event: DragEvent) {
+    console.log(event);
+  }
+
+  dragEndHandler(_: DragEvent) {
+    console.log("DragEnd");
+  }
+
+  configure() {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragend", this.dragEndHandler);
+  }
 
   renderContent() {
     this.element.querySelector("h2")!.textContent = this.project.title;
