725ebb3bc76092d82bf006e81b5cd338beafd20a
Aleksandar Ristic <storiaca@gmail.com>
2025-08-27 15:19:36 +0200

    Using ES Modules

--- 014-modules-namespaces/src/decorators/autobind.ts
+++ 014-modules-namespaces/src/decorators/autobind.ts
@@ -1,15 +1,13 @@
-namespace App {
-  /* Autobind decorator */
-  export function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
+/* Autobind decorator */
+export function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
 
-    return adjDescriptor;
-  }
+  return adjDescriptor;
 }
