e9de70facd15b6c130d82d712ea96c6dff76a517
Aleksandar Ristic <storiaca@gmail.com>
2025-09-02 19:51:45 +0200

    Runtime vs Compile-time Types with Zod

--- 017-3rd-lib-ts/app.ts
+++ 017-3rd-lib-ts/app.ts
@@ -15,6 +15,13 @@ const dataSchema = z.object({
   values: z.array(z.union([z.string(), z.number()])),
 });
 
+type Data = z.infer<typeof dataSchema>;
+
+function output(data: Data) {
+  console.log(data);
+}
+
 const content = JSON.parse(fs.readFileSync("data.json").toString());
 
 const parsedData = dataSchema.parse(content);
+output(parsedData);
