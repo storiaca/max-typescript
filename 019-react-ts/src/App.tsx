3e4aef07552cc4f2033ec1d695a0dbc92022b089
Aleksandar Ristic <storiaca@gmail.com>
2025-09-07 12:26:55 +0200

    Adding Another Components With Props

--- 019-react-ts/src/App.tsx
+++ 019-react-ts/src/App.tsx
@@ -1,4 +1,6 @@
 import Header from "./components/Header";
+import CourseGoals from "./components/CourseGoals";
+
 import goalsImg from "./assets/goals.jpg";
 
 function App() {
@@ -7,6 +9,20 @@ function App() {
       <Header image={{ src: goalsImg, alt: "List of goals" }}>
         <h1>Your Course Goals</h1>
       </Header>
+      <CourseGoals
+        goals={[
+          {
+            id: 1,
+            title: "Learn TS React",
+            description: "Learn TS from the ground up.",
+          },
+          {
+            id: 2,
+            title: "Practice TS",
+            description: "Practice working with TypeScript",
+          },
+        ]}
+      />
     </main>
   );
 }
