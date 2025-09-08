6577fa7d29ec96854ede96f1be3a4bba943e6544
Aleksandar Ristic <storiaca@gmail.com>
2025-09-08 18:42:28 +0200

    Handling Form Submission

--- 019-react-ts/src/components/NewGoal.tsx
+++ 019-react-ts/src/components/NewGoal.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import { type FormEvent } from "react";
 
 const NewGoal = () => {
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  }
   return (
-    <div>
-      <h1>New Goal</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label htmlFor="goal">Your goal</label>
+        <input id="goal" type="text" />
+      </p>
+      <p>
+        <label htmlFor="summary">Short summary</label>
+        <input id="summary" type="text" />
+      </p>
+      <p>
+        <button>Add Goal</button>
+      </p>
+    </form>
   );
 };
 
